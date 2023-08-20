c830e35e5bd67ae90929a3e6749351d9aec6e289
NayanPatidar <88453419+NayanPatidar@users.noreply.github.com>
2023-08-20 20:44:11 +0530

    MovementChecking

---
+++ ProjectPractice/MovementCheck.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import javax.swing.plaf.IconUIResource;
+import javax.tools.Tool;
+import java.awt.*;
+
+public class MovementCheck {
+    public static void main(String[] args) throws InterruptedException {
+
+
+
+//        Toolkit toolkit = Toolkit.getDefaultToolkit();
+//
+//        Dimension sizeOfScreen = toolkit.getScreenSize();
+//
+//        int screenWidth = sizeOfScreen.width;
+//        int screenHeight = sizeOfScreen.height;
+//        System.out.println(screenHeight + " " + screenWidth);
+//
+//        while (true){
+//            Point x = MouseInfo.getPointerInfo().getLocation();
+//        System.out.println(x.x);
+//        System.out.println(x.y);
+//        Thread.sleep(80);
+//            System.out.println();
+//    }
+
+
+//        while (true) {
+//
+//            Point cursorLocation = java.awt.MouseInfo.getPointerInfo().getLocation();
+//            Rectangle screenBOunds = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
+//
+//            boolean cursorWithinBounds = screenBOunds.contains(cursorLocation);
+//
+//            if (cursorWithinBounds) {
+//                System.out.println("Yes");
+//            } else if (!cursorWithinBounds) {
+//                System.out.println("No");
+//            }
+//        }
+    }
+}
