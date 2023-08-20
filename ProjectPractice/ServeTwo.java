84b793a36e389ced947f9504e8d59addf8daaa26
NayanPatidar <88453419+NayanPatidar@users.noreply.github.com>
2023-08-20 20:45:39 +0530

    Imp Server Two

---
+++ ProjectPractice/ServeTwo.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServeTwo {
+    public static void main(String[] args) {
+        int x, y;
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(12345);
+
+            while (true) {
+                Socket waitForConnectionWithClient = serverSocket.accept(); // It will wait for the client to join
+                System.out.println("Connection Accepted !!!");
+                PrintWriter printWriter = new PrintWriter(waitForConnectionWithClient.getOutputStream(), true);
+
+                while (true){
+                    x = (int)java.awt.MouseInfo.getPointerInfo().getLocation().getX();
+                    y = (int)java.awt.MouseInfo.getPointerInfo().getLocation().getY();
+
+                    String msg = x + " " + y;
+                    printWriter.println(msg);
+
+                }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
