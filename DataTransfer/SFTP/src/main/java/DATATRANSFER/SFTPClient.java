4688655721a9dc33f1c182a97b3e4c6793c8607b
NayanPatidar <nayanpatidar28@gmail.com>
2023-08-22 18:17:38 +0530

    Adding the Basic JSch file

---
+++ DataTransfer/SFTP/src/main/java/DATATRANSFER/SFTPClient.java
@@ -0,0 +1,27 @@
+package DATATRANSFER;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+public class SFTPClient {
+    public static void main(String[] args) {
+        JSch jSch = new JSch();
+
+        try {
+            Session session = jSch.getSession("Nayan", "10.200.233.92", 22 );
+            session.setPassword("Indore123$");
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+            System.out.println("Started");
+
+            if (session.isConnected()){
+                System.out.println("Is connected");
+            } else if (!session.isConnected()) {
+                System.out.println("Not connected");
+            }
+        } catch (JSchException e) {
+            System.out.println("Error Occurred");
+        }
+    }
+}
