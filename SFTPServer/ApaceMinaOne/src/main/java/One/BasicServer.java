6395c12ffdff6fdcbe7b67a48f569da5b5e87fdf
Nayan Patidar <nayanpatidar28@gmail.com>
2023-08-23 22:24:40 +0530

    Adding Files

--- SFTPServer/ApaceMinaOne/src/main/java/One/BasicServer.java
+++ SFTPServer/ApaceMinaOne/src/main/java/One/BasicServer.java
@@ -1,6 +1,8 @@
 package One;
 import org.apache.sshd.server.SshServer;
 import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
+import org.apache.sshd.server.shell.InvertedShell;
+import org.apache.sshd.server.shell.ProcessShellFactory;
 
 
 import java.io.File;
@@ -11,19 +13,23 @@ import java.util.Collections;
 
 
 public class BasicServer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         SshServer sshd = SshServer.setUpDefaultServer();
         sshd.setPort(8080);
         Path path = Paths.get("/media/nayan/Core 1/Projects/testing/Java-In-Depth/SFTPServer/ApaceMinaOne/src/main/java/One/KEY");
         sshd.setKeyPairProvider(new SimpleGeneratorHostKeyProvider(path));
+//        sshd.setShellFactory(new InvertedShell.Factory());
+        sshd.setShellFactory(new ProcessShellFactory());
+
         sshd.setPasswordAuthenticator((username, password, session) -> username.equals("test") && password.equals("password"));
-        try {
-            sshd.start();
-            System.out.println("SFTP server started");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+//            while (true) {
+//
+//            }
+        sshd.start();
+        System.out.println("SFTP server started");
+        Thread.sleep(Long.MAX_VALUE);
 
     }
 }
