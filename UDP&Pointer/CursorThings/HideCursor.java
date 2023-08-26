0e9f39edabd88fd229556c29474f98e769c08a0f
Nayan Patidar <nayanpatidar28@gmail.com>
2023-08-26 22:23:49 +0530

    Adding Files and Refracting some the Mouse Clicks Testing

---
+++ UDP&Pointer/CursorThings/HideCursor.java
@@ -0,0 +1,64 @@
+package CursorThings;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
+
+public class HideCursor {
+	public void FrameCreation(boolean visibility, JFrame frame, Toolkit toolkit, String BreakOrNot) throws AWTException {
+		Robot robot = new Robot();
+
+		if (visibility) {
+			Image blankImage = Toolkit.getDefaultToolkit().createImage(new byte[0]);
+			Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(blankImage, new Point(0, 0), "blankCursor");
+
+			frame.setCursor(blankCursor);
+			frame.setOpacity(0.004f);
+			frame.setVisible(true);
+			frame.setAlwaysOnTop(true);
+
+
+			robot.mouseMove(700, 300);
+			try {
+				Thread.sleep(1000000);
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+
+		} else if (!visibility) {
+			frame.setVisible(false);
+
+			frame.dispose();
+		}
+
+
+//        if (Objects.equals(BreakOrNot, "Break")) {
+//
+//            System.out.println("In Break");
+//            frame.dispose();
+//
+//        } else if (Objects.equals(BreakOrNot, "Don'tBreak")) {
+//            if (frame.isActive()){
+//                System.out.println("Already Present");
+//            } else if (!frame.isActive()) {
+//                frame.dispose();
+//                System.out.println("In Don't Break");
+//                Dimension screenSize = toolkit.getScreenSize();
+//
+//                int screenWidth = screenSize.width;
+//                int screenHeight = screenSize.height;
+//                frame.setSize(screenWidth, screenHeight);
+//
+//                frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+//                Image blankImage = Toolkit.getDefaultToolkit().createImage(new byte[0]);
+//                Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(blankImage, new Point(0, 0), "blankCursor");
+//                frame.setCursor(blankCursor);
+//                                frame.setUndecorated(true);
+//                frame.setOpacity(0.004f);
+//                frame.setVisible(visibility);
+//            }
+
+	}
+	// Get the screen size
+
+}
