ddfc665b9db23b26e81051b8ee3e091f7665dab9
NayanPatidar <88453419+NayanPatidar@users.noreply.github.com>
2023-08-27 20:56:49 +0530

    Update MouseClickTracker.java

--- MouseGUI/MousePlusGUI/src/main/java/ConcurrentThreadsOne/MouseClickTracker.java
+++ MouseGUI/MousePlusGUI/src/main/java/ConcurrentThreadsOne/MouseClickTracker.java
@@ -5,36 +5,28 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-public class MouseClickTracker extends JFrame {
+public class MouseClickTracker {
+	private JFrame jFrame;
 
-	private JLabel infoLabel;
+			public MouseClickTracker(JFrame frame) {
+				this.jFrame = frame;
 
-	public MouseClickTracker(JFrame frame) {
-		setTitle("Mouse Click Tracker");
-		setSize(400, 300);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		infoLabel = new JLabel("No mouse clicks yet.");
-		infoLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		add(infoLabel, BorderLayout.CENTER);
-
-		frame.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				int x = e.getX();
-				int y = e.getY();
-				String clickInfo = "Mouse clicked at X: " + x + ", Y: " + y;
-				if (e.getButton() == MouseEvent.BUTTON1) {
-					clickInfo += " (Left button)";
-				} else if (e.getButton() == MouseEvent.BUTTON2) {
-					clickInfo += " (Middle button)";
-				} else if (e.getButton() == MouseEvent.BUTTON3) {
-					clickInfo += " (Right button)";
-				}
-				infoLabel.setText(clickInfo);
+				jFrame.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent e) {
+							int x = e.getX();
+							int y = e.getY();
+							String clickInfo = "Mouse clicked at X: " + x + ", Y: " + y;
+							if (e.getButton() == MouseEvent.BUTTON1) {
+								clickInfo += " (Left button)";
+							} else if (e.getButton() == MouseEvent.BUTTON2) {
+								clickInfo += " (Middle button)";
+							} else if (e.getButton() == MouseEvent.BUTTON3) {
+								clickInfo += " (Right button)";
+							}
+							System.out.println(clickInfo);
+					}
+				});
 			}
-		});
 	}
 
-	// Rest of the class remains the same
-}
